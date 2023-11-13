7abdf235ac956dafaa551383fa838c884ddb55d0
hidekiiwasa <Spider321011@hotmail.com>
2023-11-12 23:43:38 -0300

    sad

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,23 +1,56 @@
 import { useState, useEffect } from 'react'
 import '../styles/components/_login.css'
+import loginData from '../login.json'
 
 function Login() {
+
+    const [loginDetails, setLoginDetails] = useState({ email: '', senha: '' });
+
+    const handleChange = e => {
+        setLoginDetails({ ...loginDetails, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+
+        console.log("Email digitado:", loginDetails.email);
+        console.log("Senha digitada:", loginDetails.senha);
+        
+        e.preventDefault();
+
+        const usuarioEncontrado = loginData.login.find(
+        user => user.email === loginDetails.email && user.senha === loginDetails.senha
+        )
+
+
+
+
+        if (usuarioEncontrado) {
+        alert('Login bem-sucedido!');
+        window.location='/'
+
+        } else {
+        alert('Credenciais inválidas. Tente novamente.');
+
+        }
+    };
+
+    
     
     return(
         <>
             <div className="logincontainer">
                 <div className="loginconteudo">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="inputcontainer">
                             <label htmlFor='email'>email:</label>
-                            <input id="email" type="email" placeholder='email@gmail.com'/>
+                            <input id="email" onChange={handleChange} type="email" name='email' placeholder='email@gmail.com'/>
                         </div>
                         <div className="inputcontainer">
                             <label htmlFor='senha'>senha:</label>
-                            <input id="senha" type="password" placeholder='12345678xxx'/>
+                            <input id="senha" onChange={handleChange} type="password" name='senha' placeholder='12345678xxx'/>
                         </div>
                         <div className="botaologin">
-                            <button>LOGIN</button>
+                            <button type='submit'>LOGIN</button>
                             <a href="/cadastro">Criar uma conta?</a>
                         </div>
                     </form>
