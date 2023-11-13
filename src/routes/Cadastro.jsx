0e479efef842be60ac942ebe2e28dc840c364f72
hidekiiwasa <Spider321011@hotmail.com>
2023-11-12 21:04:21 -0300

    api

--- src/routes/Cadastro.jsx
+++ src/routes/Cadastro.jsx
@@ -3,14 +3,29 @@ import { useState } from 'react'
 
 function Cadastro() {
 
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [novo, setNovo] = useState({
+        email:"",
+        senha:"",
+    })
+    
+    const handleChange =(e)=>{
+        setNovo({...novo, [e.target.name]:e.target.value});
+    };
 
-    const handleSubmit = (e) => {
+    const handleSubmit =(e)=>{
         e.preventDefault();
-        localStorage.setItem("email", email)
-        localStorage.setItem("senha", pass)
+
+        fetch("http://localhost:5000/login/",{
+        method:'post',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(novo),
+        }).then(()=>{
+        window.location='/';
+        }), [];
     }
+
     return(
         <>
             <div className="cadastrocontainer">
@@ -18,14 +33,14 @@ function Cadastro() {
                     <form onSubmit={handleSubmit}>
                         <div className="inputcontainer">
                             <label htmlFor='email'>email:</label>
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} id="email"type="email" placeholder='email@gmail.com'/>
+                            <input value={novo.email} name='email' onChange={handleChange} id="email"type="email" placeholder='email@gmail.com'/>
                         </div>
                         <div className="inputcontainer">
                             <label htmlFor='senha'>senha:</label>
-                            <input value={pass} onChange={(e) => setPass(e.target.value)} id="senha" type="password" placeholder='12345678xxx'/>
+                            <input value={novo.senha} name='senha' onChange={handleChange} id="senha" type="password" placeholder='12345678xxx'/>
                         </div>
                         <div className="botaologin">
-                            <button>CRIAR</button>
+                            <button type='submit'>CRIAR</button>
                             <a href="/login">Já tem conta?</a>
                         </div>
                     </form>
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,36 +1,20 @@
-import { } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/components/_login.css'
 
 function Login() {
     
-    
-    const emailCadastro = localStorage.getItem("email");
-    const senhaCadastro = localStorage.getItem("senha");
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const emailLogin = document.getElementById('email').value
-        const senhaLogin = document.getElementById('senha').value
-        if(emailCadastro === emailLogin && senhaCadastro === senhaLogin) {
-            alert("Login efetuado")
-        } else {
-            alert("Login incorreto")
-        }
-    }
-
     return(
         <>
             <div className="logincontainer">
                 <div className="loginconteudo">
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         <div className="inputcontainer">
                             <label htmlFor='email'>email:</label>
-                            <input onChange={(e) => setEmail(e.target.value)} id="email" type="email" placeholder='email@gmail.com'/>
+                            <input id="email" type="email" placeholder='email@gmail.com'/>
                         </div>
                         <div className="inputcontainer">
                             <label htmlFor='senha'>senha:</label>
-                            <input onChange={(e) => setPass(e.target.value)} id="senha" type="password" placeholder='12345678xxx'/>
+                            <input id="senha" type="password" placeholder='12345678xxx'/>
                         </div>
                         <div className="botaologin">
                             <button>LOGIN</button>
